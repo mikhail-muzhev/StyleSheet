c12a913584f9ac7763a7664247914d26f8203e62
Mikhail Muzhev <mikhail.muzhev@ltst.ru>
2019-03-28 08:55:16 +0600

    Initial Commit

---
+++ StyleSheet/StyleSheet.h
@@ -0,0 +1,19 @@
+//
+//  StyleSheet.h
+//  StyleSheet
+//
+//  Created by Mikhail Muzhev on 28/03/2019.
+//  Copyright © 2019 Mikhail Muzhev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StyleSheet.
+FOUNDATION_EXPORT double StyleSheetVersionNumber;
+
+//! Project version string for StyleSheet.
+FOUNDATION_EXPORT const unsigned char StyleSheetVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StyleSheet/PublicHeader.h>
+
+
